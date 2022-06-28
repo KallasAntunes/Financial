919c4c123b6705ad8c1d7cdd637d50df642826b4
kallas <kallas.lima@in8.com.br>
2022-06-27 21:57:01 -0300

    Removed unused boilerplate

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,9 +1,30 @@
+import { InputData } from '@/interfaces/calculator_input.interface';
 import { NextFunction, Request, Response } from 'express';
 
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
-      res.sendStatus(200);
+      const data: InputData = {
+        initialValue: Number(req.query.initialValue),
+        monthlyContribution: Number(req.query.monthlyContribution),
+        increaseFrequency: Number(req.query.increaseFrequency),
+        increaseTotalAmount: Number(req.query.increaseTotalAmount),
+        increasePercentage: Number(req.query.increasePercentage),
+        desiredTime: Number(req.query.desiredTime),
+        profitability: Number(req.query.profitability),
+        desiredDividends: Number(req.query.desiredDividends),
+      };
+
+      //Validating input data
+      const errors: string[] = Object.entries(data).map(([key, value]) => {
+        if (Number.isNaN(value)) return `${key} is required`;
+      });
+      if (errors.length > 0) {
+        res.status(300).send(errors);
+        return;
+      }
+
+      res.status(200).send(data);
     } catch (error) {
       next(error);
     }
---
+++ src/interfaces/calculator_input.interface.ts
@@ -0,0 +1,10 @@
+export interface InputData {
+  initialValue: number;
+  monthlyContribution: number;
+  increaseFrequency: number;
+  increaseTotalAmount: number;
+  increasePercentage: number;
+  desiredTime: number;
+  profitability: number;
+  desiredDividends: number;
+}
